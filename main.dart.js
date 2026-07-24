6e4e3754ba2011a8dcc378221e3140934be92482
cavepv <cavepv@users.noreply.github.com>
2026-07-24 21:18:38 +0000

    deploy: dea4d75447e70c543ed1ea7321a7d8d1a112258a

--- main.dart.js
+++ main.dart.js
@@ -22367,7 +22367,7 @@ $iE0:1}
 A.abN.prototype={
 $1(a){var s=A.cl().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/69c8c61792f04cc809dfef0c910414fb9afc06cd/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/0cd610717bde95fd88343c64f81c11ba4e5c0010/":s)+a},
 $S:164}
 A.Eg.prototype={
 gq(a){var s=this.a
